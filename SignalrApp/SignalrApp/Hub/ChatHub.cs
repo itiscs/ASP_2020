1417e5dfe58a351d8c2cd5c00d0ee493212f7342
moudroff <moudroff>
2020-10-01 15:08:45 +0300

    Web Api 1

---
+++ SignalrApp/SignalrApp/Hub/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalrApp.Hub
+{
+    public class ChatHub: Microsoft.AspNetCore.SignalR.Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
---
+++ WebApiApp/WebApiApp/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+       
+
+    }
+
+
+}
