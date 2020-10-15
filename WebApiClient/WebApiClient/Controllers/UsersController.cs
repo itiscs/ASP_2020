02fadc9c968416dec0fd87df5894398f5575da01
moudroff <moudroff>
2020-10-15 15:04:38 +0300

    WebApiClient

---
+++ WebApiClient/WebApiClient/Controllers/UsersController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiClient.Models;
+using WebApiClient.Services;
+
+namespace WebApiClient.Controllers
+{
+    public class UsersController : Controller
+    {
+        UsersService serv = new UsersService();
+
+        // GET: Users
+        public async Task<ActionResult> Index()
+        {
+            return View(await serv.GetUsers());
+        }
+
+        // GET: Users/Details/5
+        public async Task<ActionResult> Details(int id)
+        {
+
+            return View(await serv.GetUsersByID(id));
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind("Name", "Age")]User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    await serv.AddUser(user);
+                    return RedirectToAction(nameof(Index));
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
